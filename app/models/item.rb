874c1ef2016c2bd0b5e196275c9de87b63032a36
kenzamu <jbantian553@gmail.com>
2023-04-27 13:52:23 +0900

    エラーメッセージが表示されるように修正

--- app/models/item.rb
+++ app/models/item.rb
@@ -7,8 +7,9 @@ class Item < ApplicationRecord
   belongs_to :delivery_day
 
   validates :name, :description, presence: true
-  validates :price, presence: true, numericality: {only_integer: true, greater_than_or_equal_to: 300, less_than_or_equal_to: 9999999},
-                    format: { with: /\A[0-9]+\z/ }
+  validates :price, presence: true, numericality: {only_integer: true, greater_than_or_equal_to: 300, less_than_or_equal_to: 9999999, message: "is out of setting range"},
+                    format: { with: /\A[0-9]+\z/, message: "is invalid. Input half-width characters"}
+  validates :image, presence: true
 
   validates :category_id, numericality: { other_than: 1 , message: "can't be blank" } 
   validates :condition_id, numericality: { other_than: 1 , message: "can't be blank" } 
@@ -17,4 +18,5 @@ class Item < ApplicationRecord
   validates :delivery_day_id, numericality: { other_than: 1 , message: "can't be blank" } 
 
   belongs_to :user
+  has_one_attached :image
 end
